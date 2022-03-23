9369b22485b731f31f3e08fb61a9324dcdb18fe1
farhad-habibi <farhadmalekzada@gmail.com>
2022-03-23 09:21:20 +0430

    created palettet.js component as well as seedColors file to display colors

---
+++ src/Palette.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Palette extends Component {
+    render() {
+        return (
+            <div className="Palette">
+                {/* navbar component */}
+                <div className="Palette-colors">
+                    {/* color box component */}
+                </div>
+                {/* footer */}
+            </div>
+        )
+    }
+}
+
+export default Palette;
