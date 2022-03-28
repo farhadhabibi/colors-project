3729743c34de797aec7ea08d433409049b9a96eb
farhad-habibi <farhadmalekzada@gmail.com>
2022-03-28 11:09:57 +0430

    installed and fixed palette routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import { Routes, Route, useParams } from 'react-router-dom';
 import Palette from './Palette';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelper';
 import './App.css';
 
 function App() {
-
+  const FindPaletteId = () => {
+    const { id } = useParams();
+    const palette = seedColors.find(palette => palette.id === id)
+    return <Palette palette={generatePalette(palette)} />
+  }
   return (
-    <div>
-      <Palette palette={generatePalette(seedColors[4])} />
-    </div>
+    <Routes>
+      <Route path="/" element={<h1>main list</h1>} />
+      <Route path="/palette/:id" element={<FindPaletteId />}></Route>
+    </Routes>
   );
 }
 
